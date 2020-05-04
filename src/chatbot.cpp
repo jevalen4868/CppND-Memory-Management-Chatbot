841eb6fc684a7d384a780aa0b01d5f79d1f7e382
Jeremy Valenzuela <jevalen4868@yahoo.com>
2020-05-03 20:08:21 -0500

    chatbot allocation on stack, not heap. debugging

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -56,14 +56,14 @@ ChatBot &ChatBot::operator=(const ChatBot &source) {
     std::cout << "ChatBot copy assignment constructor" << std::endl;
     if(this == &source) { return *this; }
 
-    /*_chatLogic = nullptr;
+    _chatLogic = nullptr;
     _rootNode = nullptr;
-    _currentNode = nullptr;*/
+    //_currentNode = nullptr;
 
     _image = source._image;
     _chatLogic = source._chatLogic;
     _rootNode = source._rootNode;
-    _currentNode = source._currentNode;
+    //_currentNode = source._currentNode;
     return *this;
 }
 
@@ -72,28 +72,28 @@ ChatBot::ChatBot(ChatBot &&source) {
     _image = source._image;
     _chatLogic = source._chatLogic;
     _rootNode = source._rootNode;
-    _currentNode = source._currentNode;
+    //_currentNode = source._currentNode;
 
-    /*source._chatLogic = nullptr;
+    source._chatLogic = nullptr;
     source._rootNode = nullptr;
-    source._currentNode = nullptr;*/
+    //source._currentNode = nullptr;
 }
 
 ChatBot &ChatBot::operator=(ChatBot &&source) {
     std::cout << "ChatBot move assignment constructor" << std::endl;
     if(this == &source) { return *this; }
 
-    //_chatLogic = nullptr;
-    //_rootNode = nullptr;
+    _chatLogic = nullptr;
+    _rootNode = nullptr;
     //_currentNode = nullptr;
 
     _image = source._image;
     _chatLogic = source._chatLogic;
     _rootNode = source._rootNode;
-    _currentNode = source._currentNode;
+    //_currentNode = source._currentNode;
 
-    //source._chatLogic = nullptr;
-    //source._rootNode = nullptr;
+    source._chatLogic = nullptr;
+    source._rootNode = nullptr;
     //source._currentNode = nullptr;
 
     return *this;
